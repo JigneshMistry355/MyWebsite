4b24ca2d4a5611cc5a8aed26208b8c425b663871
Jignesh <jvmistry7@gmail.com>
2025-01-23 11:28:31 +0530

    websocket client server update
    Asynchronous messaging working fine (without sender label)

--- backend/testapi/client.py
+++ backend/testapi/client.py
@@ -1,11 +1,30 @@
-import asyncio
-from websockets.asyncio.client import connect
+import asyncio, websockets
+import sys
 
-async def hello():
-    async with connect("ws://localhost:8765") as websocket:
-        await websocket.send("Hello")
-        message = await websocket.recv()
-        print(message)
+async def chat(name):
+    async with websockets.connect('ws://localhost:8765') as websocket:
+        
+        send_task = asyncio.create_task(sendMessage(websocket, name))
+        receive_task = asyncio.create_task(receiveMessage(websocket))
+        await asyncio.gather(send_task, receive_task)
+        
+async def sendMessage(websocket, c_name):
+    while True:
+        message = await asyncio.get_event_loop().run_in_executor(None, input, f"{c_name} : ")
+        await websocket.send(message)
+
+async def receiveMessage(websocket):
+    while True:
+        response = await websocket.recv()
+        print(f'Received : {response}')
+
+
+
+async def main():
+    name = sys.argv[1] if len(sys.argv) > 1 else "Client"
+    await chat(name)
 
 if __name__ == '__main__':
-    asyncio.run(hello())
+    asyncio.run(main())
+
+
--- backend/testapi/server.py
+++ backend/testapi/server.py
@@ -1,13 +1,67 @@
-import asyncio
+import asyncio, websockets
 from websockets.asyncio.server import serve
+from websockets.asyncio.client import connect
+from websockets.exceptions import ConnectionClosedError
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
+# set of coonected clients
+connected_clients = set()
 
-async def main():
-    async with serve(echo, "localhost", 8765) as server:
-        await server.serve_forever()
+# Function to handle each client connection
+async def handler(websocket):
+
+    # name = await websocket.recv()
+    # connected_clients[websocket] = name
+
+    print("Websocket : server started")
+
+    # websocket contains (name/connection) of the client 
+    if websocket not in connected_clients:
+        connected_clients.add(websocket)
+
+    try:
+        # Listen for messages from clients
+        async for message in websocket:
+
+
+            # print(f"Received message from {name}: {message}")
+            # Broadcast messages to all other connected clients
+            # Ensures message is not sent back to sender
+            # websockets.broadcast(connected_clients, message)
+
+            for client in connected_clients:
+                 if client != websocket:
+                      await client.send(message)
+            # await asyncio.gather(
+            #     *[client.send(message) for client in connected_clients if client != websocket]
+            # )
     
+    finally:            
+            #Remove the client from the set
+            connected_clients.remove(websocket)
+    
+async def main():
+     async with websockets.serve(handler, 'localhost', 8765) as server:
+        # await server.serve_forever()
+        await asyncio.Future()
+
+
 if __name__ == '__main__':
     asyncio.run(main())
+
+
+# import asyncio
+# from websockets.asyncio.server import serve
+
+
+# async def echo(websocket):
+#     async for message in websocket:
+#         await websocket.send(message)
+
+
+# async def main():
+#     async with serve(echo, "localhost", 8765) as server:
+#         await server.serve_forever()
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
